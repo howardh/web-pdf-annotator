202001e901d1c8afb81c36e02f27b700c6da60b8
Howard <howard.huang@mail.mcgill.ca>
2021-04-12 15:10:42 -0400

    Hide rect controls when clicking away

--- web/src/PdfViewer.js
+++ web/src/PdfViewer.js
@@ -441,7 +441,7 @@ function Annotation(props) {
           </>
         }
         {
-          isActive && 
+          selected && 
           <AnnotationActions annotation={annotation} viewNote={viewNote}/>
         }
       </div>;
